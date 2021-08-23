265ff8fcfcc38ab9a589f957e9885a6624b35d59
HeySkidee <harshitojha46@gmail.com>
2021-08-23 11:02:19 +0530

    pyramid patternques

---
+++ cpp prgrms/9. inverted half pyramid.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int n;
+    cin>>n;
+
+    /* my code:
+
+    for(int i=0; i<=n; n--){
+        for (int j = 1; j <= n; j++)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+
+    */
+
+    // tutorial code:
+
+    for(int i=n; i>=1; i--){
+        for(int j=1; j<=i; j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+
+  return 0;
+}
