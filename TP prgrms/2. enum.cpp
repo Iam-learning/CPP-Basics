d65f43fe5c53b6e001de5c2b9045c1075ed21708
HeySkidee <harshitojha46@gmail.com>
2021-08-31 08:12:35 +0530

    macros and enums

--- TP prgrms/2. enum.cpp
+++ TP prgrms/2. enum.cpp
@@ -1,14 +1,29 @@
 #include <iostream>
 using namespace std;
 
-enum week {mon, tue, wed, thurs, fri, sat, sun};
+enum week {mon, tue, wed, thurs=6, fri, sat, sun};
 
 
 int main(){
 
     enum week day;
     day = wed;
-    cout<<day;
+    cout<<day<<endl;
+    cout<<thurs<<endl;
+    cout<<fri<<endl;
+
+    /*
+    There are multiple advantages of using enum over macro when many related named constants have integral values.
+    a) Enums follow scope rules.
+    b) Enum variables are automatically assigned values. Following is simpler
+    */
+
+   /*Macros be like:
+    #define Working 0
+    #define Failed 1
+    #define Freezed 2
+    */
+
 
     return 0;
 }
