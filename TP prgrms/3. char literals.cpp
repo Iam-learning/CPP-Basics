df36695d5dc8a43ce6418018b73f245e991ab575
HeySkidee <harshitojha46@gmail.com>
2021-08-31 18:56:47 +0530

    constansts/literals

---
+++ TP prgrms/3. char literals.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+
+    // they work with backslash only.
+
+    cout << "he\\him" << endl;
+    
+    cout << "he\'s on vacation" <<endl;
+
+    cout << "he said \"aight imma head out\" "<<endl;
+
+    return 0;
+}
